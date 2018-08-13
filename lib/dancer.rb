70ae4a31cb0a6dad00969ed6e8699fca30b72ab7
Malcome Fein <mfein90@gmail.com>
2018-08-13 03:49:42 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,4 +2,11 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+  include Dance
+ # include allow classes to use all of included methods as instance varables
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
